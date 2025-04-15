a09429e925b36d779ed5b6a168206db769d8b2b5
league <league@imac-34.attlocal.net>
2025-04-14 17:03:03 -0700

    I did memory game

--- src/memoryGame.java
+++ src/memoryGame.java
@@ -1,5 +1,12 @@
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -7,7 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class memoryGame {
+public class memoryGame implements ActionListener {
 
 	JFrame frame;
 	JPanel mainPanel;
@@ -15,10 +22,21 @@ public class memoryGame {
 	JPanel imagePanel;
 	JButton card1;
 	JButton card2;
+	JButton card3;
+	JButton card4;
+	JButton card5;
 	JLabel image1 = createLabelImage("2Diamonds.png");
-	JLabel image2 = createLabelImage("3Clubs.jpg");
+	JLabel image2 = createLabelImage("3ofClubs.jpg");
+	JLabel image3 = createLabelImage("9Diamonds.jpg");
+	JLabel image4 = createLabelImage("10Hearts.jpg");
+	JLabel image5 = createLabelImage("8Spades.jpg");
+	ArrayList<JLabel> list = new ArrayList<>();
+	List<String> list2 = Arrays.asList("");
+
 	
 	public void setup() {
+		
+		
 		
 		frame = new JFrame();
 		frame = new JFrame();
@@ -29,6 +47,12 @@ public class memoryGame {
 		imagePanel = new JPanel();
 		card1 = new JButton();
 		card2 = new JButton();
+		card3 = new JButton();
+		card4 = new JButton();
+		card5 = new JButton();
+		
+		GridLayout layout = new GridLayout(2,4);
+		secondPanel.setLayout(layout);
 		
 		image2.setSize(new Dimension(10,10));
 		
@@ -36,8 +60,16 @@ public class memoryGame {
 		mainPanel.add(secondPanel);
 		secondPanel.add(card1);
 		secondPanel.add(card2);
+		secondPanel.add(card3);
+		secondPanel.add(card4);
+		secondPanel.add(card5);
 		card1.add(image1);
 		card2.add(image2);
+		card3.add(image3);
+		card4.add(image4);
+		card5.add(image5);
+		
+		card1.addActionListener(this);
 		
 	
 		
@@ -55,6 +87,17 @@ public class memoryGame {
 		JLabel imageLabel = new JLabel(icon);
 	
 		return imageLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Random ran = new Random();
+		JButton buttonPressed = (JButton) e.getSource();
+		
+		if (buttonPressed.equals(card1)) {
+			
+		}
 	}
 	
 	
